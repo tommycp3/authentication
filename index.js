45708079660768439bf146035e9f11d5141bc3c9
Thomas Miller <thomas.miller@consensys.net>
2018-06-27 22:40:45 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+
+
+// App Setup app.use registers morgan and body parser as middleware.
+app.use(morgan('combined'));
+app.use(bodyParser.json( {type: '*/* ' }));
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on: ', port);
+
