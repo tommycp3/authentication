a9abf90eb4975bb8a50ee1ef76f4a9e438ecb0ee
Thomas Miller <thomas.miller@consensys.net>
2018-07-03 12:52:58 +1000

    add localhost/signup authentication controler

--- router.js
+++ router.js
@@ -1,5 +1,7 @@
+const Authentication = require('./controlers/authentication');
+
 module.exports = function(app) {
-    app.get('/', function(req, res, next) {
-        res.send(['waterbottle', 'phone', 'paper']);
-    });
-}
+    app.post('/signup', Authentication.signup);
+    
+     
+};
